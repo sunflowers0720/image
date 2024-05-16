e81a9440556dd07da76d57b6be37949978bd1c9d
sunflowers0720 <167154233+sunflowers0720@users.noreply.github.com>
2024-05-16 18:14:36 +0800

    Add files via upload

---
+++ Data operation/Data preprocessing.py
@@ -0,0 +1,5 @@
+transform = transforms.Compose([
+    transforms.Resize((32, 32)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
---
+++ Data operation/Load data.py
@@ -0,0 +1,5 @@
+trainset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=100, shuffle=True, num_workers=2)
+
+testset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
